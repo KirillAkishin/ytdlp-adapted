3cc330d02e7d998a560d9a007d1f6382b9c7170d
KirillAkishin <akishin.ka@yandex.ru>
2025-09-27 14:03:36 +0300

    fixed proxy-pool cycle

--- main.py
+++ main.py
@@ -13,9 +13,12 @@ if __name__ == '__main__':
     url_example = "http://ifconfig.me"
     output_path = '~/Downloads/test_dir'
 
-    for proxy in proxy_generator(expiration=15):
-        print(f"try proxy: {proxy}", end='\t')
-        download_video_simple(url_video, output_path=output_path, proxy=proxy.split("://")[1])
-        # resp = get_request(url=url_example, http_proxy_address=proxy.split("://")[1])
-        time.sleep(0.1)
+    for proxy in proxy_generator(expiration=15, repetitions=1):
+        logging.info(f"try proxy: {proxy}")
+        try: 
+            download_video_simple(url_video, output_path=output_path, proxy=proxy.split("://")[1])
+            # resp = get_request(url=url_example, http_proxy_address=proxy.split("://")[1])
+        except:
+            logging.debug(f"bad proxy: {proxy}")
+            time.sleep(0.1)
  
